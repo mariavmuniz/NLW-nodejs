2e45e441fc96573d539ab8ae973f9aa6fe3efd60
mariavmuniz <soaresmunizm@gmail.com>
2024-02-07 11:05:22 -0300

    feat(*): configuration fastify and docker

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello word")
+import fastify from "fastify"
+
+const app = fastify() //instanciando 
+
+app.get('/hello', () => {
+    return 'Hello NLW'
+})
+
+app.listen({ port: 3333 }).then( () => {
+    console.log('HTTP server running')
+})
